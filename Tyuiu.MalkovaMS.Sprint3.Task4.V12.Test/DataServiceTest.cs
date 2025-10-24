e43df4647e477564d600548c32accac1b40da381
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-10-24 19:42:13 +0500

    Task 4

---
+++ Tyuiu.MalkovaMS.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MalkovaMS.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.MalkovaMS.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int start = -5;
+            int end = 5;
+            Assert.AreEqual(467.59, ds.Calculate(start, end));
+        }
+    }
+}
---
+++ Tyuiu.MalkovaMS.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MalkovaMS.Sprint3.Task4.V12.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int StartValue = -5;
+        int StopValue = 5;
+
+        Console.Title = "Спринт #3 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции. При х = 0 прервать цикл. Полученные значения перемножать.      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Старт шага = " + StartValue);
+        Console.WriteLine("Конец шага = " + StopValue);
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Произведение ряда = " + ds.Calculate(StartValue, StopValue));
+        Console.ReadKey();
+    }
+}
